92307298167446a9b07843387998a6c372c73409
Saimul Hoque <SAIM@Ahsanuls-MacBook-Air.local>
2026-08-01 12:50:50 +0600

    Completed Front End(except Transaction part)

--- home.js
+++ home.js
@@ -3,11 +3,13 @@ const el_cashoutBtn = document.querySelector("#cashout-btn");
 const el_transferBtn = document.querySelector("#transfer-btn")
 const el_bonusBtn = document.querySelector("#bonus-btn");
 const el_addMoneyBtn = document.querySelector("#addMoney-btn");
+const el_payBillBtn = document.querySelector("#payBill-btn");
 
 const el_cashoutSection = document.querySelector("#cashout-section");
 const el_transferSection = document.querySelector("#transfer-section");
 const el_bonusSection = document.querySelector("#bonus-section");
 const el_addMoneySection = document.querySelector("#addMoney-section");
+const el_payBillSection = document.querySelector("#payBill-section");
 
 
 el_cashoutBtn.addEventListener("click", () => {
@@ -38,4 +40,11 @@ el_addMoneyBtn.addEventListener("click", () => {
     }
     el_addMoneySection.classList.remove("hidden");
     currentActive = el_addMoneySection;
+})
+el_payBillBtn.addEventListener("click", () => {
+    if(currentActive){
+        currentActive.classList.add("hidden");
+    }
+    el_payBillSection.classList.remove("hidden");
+    currentActive = el_payBillSection;
 })
